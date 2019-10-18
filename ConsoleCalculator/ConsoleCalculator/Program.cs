3cbec2e102eaf21afb3c4352278885d25a6a6e55
Shaun Meyer <shaun.meyer@dwsconsultants.com>
2019-10-18 08:45:43 +0100

    Initial commit. Add method still to be implemented.

---
+++ ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+
+    public static class Calculator
+    {
+        public static int Add(int val1, int val2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"2 + 2 = {Calculator.Add(2, 2)}");
+        }
+    }
+}
